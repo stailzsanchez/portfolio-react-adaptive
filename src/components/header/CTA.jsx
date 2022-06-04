20a865b7f66ab91bf5a077b0f3b7551dc68defa1
stailzgit <77897064+stailzgit@users.noreply.github.com>
2022-06-04 13:41:29 +0300

    fix word

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,15 +1,17 @@
-import CV from './../../assets/CV.pdf'
-import React from 'react'
+import CV from "./../../assets/Гореньков_Станислав_FrontendDev.pdf";
+import React from "react";
 
 const CTA = () => {
-    return (
-        <div className='cta'>
-            <a href={CV}
-                download
-                className='btn'>Dowload CV</a>
-            <a href="#contacts" className='btn btn-primary'>Let's Talk</a>
-        </div>
-    )
-}
+  return (
+    <div className="cta">
+      <a href={CV} download className="btn">
+        Download CV
+      </a>
+      <a href="#contacts" className="btn btn-primary">
+        Let's Talk
+      </a>
+    </div>
+  );
+};
 
-export default CTA
+export default CTA;
